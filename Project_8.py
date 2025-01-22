feec0573a7d6756561cd4209308845aee3ec79df
ceciitorres <by.cecitorres@gmail.com>
2025-01-22 23:27:43 +1300

    Add the hypothesis test to the code

--- Project_8.py
+++ Project_8.py
@@ -5,6 +5,8 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import seaborn as sns
+from scipy import stats as st
+
 
 # Import datasets
 trips_df = pd.read_csv(r"C:\Users\cecy_\OneDrive\Documentos\Data_Science\Proyectos\Project_Sprint_8\data\moved_project_sql_result_01.csv")
@@ -64,7 +66,61 @@ plt.show(block=True)
 # Conclusion
 # The neibourhood where most people is dropoff is Loop with 10,728 average trips,
 # follow it by River North and Streeterville with an average of 9,524 and 6,665 respectively
+# Los vecindarios donde la mayotia de los viajes son finalizado es Loop con una media de 10,728 vaijes,
+# seguido por River North y Streeterville con una media de 9,524 y 6,665 respectivamente
+
 # The most popular company is Flash Cab with a total of 19,558 trips, follow it
 # by Taxi Affiliation Services and Medallion Leasin with 11,422 and 10,367 respectively 
+# La compañia mas popular es Flash Cab con un total de 19,558 viajes, seguida por
+# Taxi Affiliation Services y Medallion Leasin con 11,442 y 10,367 respectivamente.
 
 
+# Prove the hypothesis 
+# "Average trip length from the Loop to O'Hare International Airport changes on rainy Saturdays."
+# "La duración promedio de los viajes desde el Loop hasta el Aeropuerto Internacional O'Hare cambia los sábados lluviosos".
+
+
+# Visualised the DataFrame
+weather_df.head(10)
+
+# Compare days
+rainy_days = weather_df[weather_df['weather_conditions'] == 'Bad']
+other_days = weather_df[weather_df['weather_conditions'] == 'Good']
+
+# Caculate average trip duration
+rainy_days_avg = rainy_days['duration_seconds'].mean()
+other_days_avg = other_days['duration_seconds'].mean()
+print(f"Average Duration on Rainy Saturdays: {rainy_days_avg} seconds")
+print(f"Average Duration on Non-Rainy Saturdays: {other_days_avg} seconds")
+
+# Perform a two-sample t-test
+t_stat, p_value = st.ttest_ind(
+    rainy_days['duration_seconds'],
+    other_days['duration_seconds'],
+    equal_var=False  # Assume unequal variance
+)
+
+print(f"T-statistic: {t_stat}")
+print(f"P-value: {p_value}")
+
+# Conclusion
+if p_value < 0.05:
+    print("The average trip duration significantly changes on rainy Saturdays.")
+else:
+    print("No significant difference in average trip duration on rainy Saturdays.")
+    
+# Hypothesis and Conclusion
+# A two-tailed test was used since with this we can consider two possibilities, 
+# that the duration is the same or different.
+# For the null hypothesis, it is considered that the average duration of trips on rainy 
+# days is the same or significantly similar to the rest of the days, therefore the 
+# alternative hypothesis suggests that the duration of trips on Saturdays without rain 
+# are significantly different from rainy days
+
+# Hipotesis ay Conclusion
+# Se utilizó una prueba de dos colas ya que con esta podemos considerar dos posibilidades, 
+# que la duración sea igual o distinta.
+# Para la hipotesis nula se considera que la media de duracion de los viajes los dias 
+# lluviosos es igual o significativamente similar a el resto de los dias, por lo tanto 
+# la hipotesis alternativa sugiere que la duracion de los viajes los Sabados sin lluvia 
+# son significativamente diferentes a los dias de lluvis
